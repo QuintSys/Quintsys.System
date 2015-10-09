9f6be77ba7e1dc1bc48b17b074400996e3fc3ec7
Erich Quintero <qbantek@gmail.com>
2015-10-09 14:14:52 -0400

    tests for object extensions

--- Quintsys.System/Tests/DecimalExtensionsTests.cs
+++ Quintsys.System/Tests/DecimalExtensionsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using static Quintsys.System.DecimalExtensions;
 
 namespace Quintsys.System.Tests
@@ -10,9 +9,7 @@ namespace Quintsys.System.Tests
         [Test]
         public void Values_Smaller_Than_SQL_Money_Minimum_Should_Be_Changed_To_SQL_Money_Minimum()
         {
-            const decimal smallDecimalValue = (SqlMoneyMinimumValue - 1.0M);
-
-            decimal result = smallDecimalValue.SqlSafeDecimalValue();
+            decimal result = decimal.MinValue.SqlSafeDecimalValue();
 
             Assert.AreEqual(expected: SqlMoneyMinimumValue, actual: result);
         }
@@ -20,19 +17,15 @@ namespace Quintsys.System.Tests
         [Test]
         public void Values_Greater_Than_SQL_Money_Maximum_Should_Be_Changed_To_SQL_Money_Maximum()
         {
-            const decimal largeDecimalValue = (SqlMoneyMaximumDecimal + 1.0M);
-
-            decimal result = largeDecimalValue.SqlSafeDecimalValue();
+            decimal result = decimal.MaxValue.SqlSafeDecimalValue();
 
             Assert.AreEqual(expected: SqlMoneyMaximumDecimal, actual: result);
         }
 
         [Test]
-        public void Values_Greater_Than_SQL_Money_Minimum_And_Smaller_Than_SQL_Money_Maximum_Should_Not_Change()
+        public void Values_Greater_Than_SQL_Money_Minimum_And_Smaller_Than_SQL_Money_Maximum_Should_Not_Change(
+            [Random((double) SqlMoneyMinimumValue, (double) SqlMoneyMaximumDecimal, 1)] decimal randomDecimalValue)
         {
-            var random = new Random();
-            decimal randomDecimalValue = random.RandomDecimal(SqlMoneyMaximumDecimal, SqlMoneyMinimumValue);
-
             decimal result = randomDecimalValue.SqlSafeDecimalValue();
 
             Assert.AreEqual(expected: randomDecimalValue, actual: result);
---
+++ Quintsys.System/Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Quintsys.System.Tests
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public static void Should_Convert_Non_Nullable_Types()
+        {
+            const int nonNullable = 1;
+            decimal converted = nonNullable.ChangeType<decimal>();
+
+            Assert.IsInstanceOf<decimal>(converted);
+        }
+
+        [Test]
+        public static void Should_Convert_Nullable_Types()
+        {
+            int? nonNullable = 1;
+            decimal? converted = nonNullable.ChangeType<decimal?>();
+
+            Assert.IsInstanceOf<decimal?>(converted);
+        }
+
+        [Test]
+        public static void Should_Return_Default_Values_For_Nulls()
+        {
+            decimal? converted = ((int?) null).ChangeType<decimal?>();
+
+            Assert.AreEqual(default(int?), converted);
+        }
+    }
+}
