4e289e2cad0440b6f542ba4e72896c1fbad6df12
Erich Quintero <qbantek@gmail.com>
2015-10-26 19:09:58 -0400

    better description

--- Quintsys.System/System/Properties/AssemblyInfo.cs
+++ Quintsys.System/System/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Quintsys.System")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("The System namespace contains fundamental classes and base classes that define commonly-used value and reference data types, events and event handlers, interfaces, attributes, and processing exceptions.")]
+[assembly: AssemblyConfiguration("Extensions and custom classes for the System namespace.")]
 [assembly: AssemblyCompany("QuintSys")]
 [assembly: AssemblyProduct("Quintsys.System")]
 [assembly: AssemblyCopyright("Copyright © QuintSys 2015")]
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0970dfb4-906e-4045-b14a-96c64fc4038c")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
